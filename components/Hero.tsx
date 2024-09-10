064c467dbf553c821fa8996fd7ff4db1cb2c3a85
Megha <rwtmeghna5432@gmail.com>
2024-09-10 17:26:04 +0530

    HERO

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -30,7 +30,7 @@ const Hero = () => {
    />
 
    <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
-  Welcome , I am Megha,<br/> a Full Stack developer
+  Welcome I am Megha,<br/>  Full Stack developer
    </p>
 
    <a href="#about">
